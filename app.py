c05385f28a26d7ec1f00a8de5422f6ccd128dbc8
wufan118580-commits <wufan118580@gmail.com>
2026-03-03 04:26:20 +0000

    Update CI\CD files

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Tencent Cloud & Cloud Studio!"
+    return "Hello from SCF!"
+
+
+# 这个函数会被云函数调用
+def main_handler(event, context):
+    from serverless_wsgi import handle_request
+
+    return handle_request(app, event, context)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=9000)
